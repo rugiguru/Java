3b686761e9b15bbe532d4eb65d4c6b1c20cb77cd
rugiguru <31921091+rugiguru@users.noreply.github.com>
2021-07-25 14:02:36 +0530

    check unique chars in string

--- StringSolutions.java
+++ StringSolutions.java
@@ -4,13 +4,27 @@ import java.util.*;
 public class StringSolutions {
     public static void main(String args[])
     {
-          System.out.println(areStringsSame("Guru", "Guru"));
+          System.out.println(isUniqueChars("Guru"));
           
     }
     
     public static boolean areStringsSame(String one, String two)
     {
         return new String(one).equals(new String(two));
+    }
+
+    // check if the given string has all unique characters with 0(n) time complexity as a constraint
+    public static boolean isUniqueChars(String str)
+    {
+        boolean[] charSet = new boolean[128];
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i);
+            if(charSet[val]){
+                return false;
+            }
+            charSet[val] = true;
+        }
+        return true;
     }
 
 }
